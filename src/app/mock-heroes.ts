4d5f1c6ab6d57f5841d50f6f3feab6d650fb62eb
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2018-05-21 11:50:25 +0200

    Commit #2
    Commit #2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Spider Man' },
+  { id: 2, name: 'Fly Machine' },
+  { id: 3, name: 'Capitan of America' },
+  { id: 4, name: 'Batman' },
+  { id: 5, name: 'Harry Poter' },
+  { id: 6, name: 'Hulk' },
+  { id: 7, name: 'Iron Man' },
+  { id: 8, name: 'Superman' },
+  { id: 9, name: 'Pokemon' },
+  { id: 10, name: 'Terminator' }
+];
